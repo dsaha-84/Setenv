e388a226daf30926a58ed15bd1c26e3fe978bb41
dsaha <dsaha.cse@gmail.com>
2017-06-15 09:36:06 +0530

    New file setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+export XAUTHORITY=/home/papu/.Xauthority
+#export JAVA_HOME=/opt/data/jdk1.7.0_79/
+export JAVA_HOME=/opt/data/jdk1.8.0_65/
+export MVN_HOME=/opt/data/apache-maven-3.3.9
+export TOMCAT_HOME=/opt/data/apache-tomcat-7.0.64
+export NODE_HOME=/opt/data/nodejs
+export PATH=$JAVA_HOME/bin:$MVN_HOME/bin:$TOMCAT_HOME/bin:$NODE_HOME/bin:$PATH
